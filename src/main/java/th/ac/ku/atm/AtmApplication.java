b80666128c7ddd045e1463a194971eab9988c301
Kornkritpawit <konsoo@hotmail.com>
2020-09-18 11:55:36 +0700

    add ad simple homepage

--- src/main/java/th/ac/ku/atm/AtmApplication.java
+++ src/main/java/th/ac/ku/atm/AtmApplication.java
@@ -3,6 +3,7 @@ package th.ac.ku.atm;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//bean
 @SpringBootApplication
 public class AtmApplication {
 
