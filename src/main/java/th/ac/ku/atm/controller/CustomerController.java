225ecb90e6bf874eebddb0e1ee14878b6adb15e9
Kornkritpawit <konsoo@hotmail.com>
2020-09-19 10:28:01 +0700

    finish bankaccount

--- src/main/java/th/ac/ku/atm/controller/CustomerController.java
+++ src/main/java/th/ac/ku/atm/controller/CustomerController.java
@@ -6,30 +6,40 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.ArrayList;
-import java.util.List;
+import th.ac.ku.atm.model.Customer;
+import th.ac.ku.atm.service.CustomerService;
 
 @Controller
+@RequestMapping("/customer")
 public class CustomerController {
 
-    private List<Customer> customers = new ArrayList<>();
 
-    @GetMapping("/customer")
+//   0. handle user request
+
+    private CustomerService customerService;
+
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @GetMapping
     public String getCustomerPage(Model model) {
 //        customers.add(new Customer(1,"Peter", 1234));
 //        customers.add(new Customer(2,"Nancy", 2345));
 //        customers.add(new Customer(3,"Rick", 3456));
 
-        model.addAttribute("allCustomers", customers);
+//        2. update model for template
+        model.addAttribute("allCustomers", customerService.getCustomers());
+        // 1. choose HTML template
         return "customer";
     }
 
-    @PostMapping("/customer")
+    @PostMapping
     public String registerRegister(@ModelAttribute Customer customer, Model model) {
-
-        customers.add(customer);
-        model.addAttribute("allCustomers", customers);
+        System.out.println(customer);
+        customerService.createCustomer(customer);
+        model.addAttribute("allCustomers", customerService.getCustomers());
         return "redirect:customer";
     }
+
 }
---
+++ src/main/java/th/ac/ku/atm/service/CustomerService.java
@@ -0,0 +1,55 @@
+package th.ac.ku.atm.service;
+
+import org.mindrot.jbcrypt.BCrypt;
+import org.springframework.stereotype.Service;
+import th.ac.ku.atm.model.Customer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CustomerService {
+// responsible for processing and managing
+//    customer information
+    private List<Customer> customerList = new ArrayList<>();
+
+    public void createCustomer(Customer customer) {
+        String hashedPin = hash(customer.getPin());
+        customer.setPin(hashedPin);
+        customerList.add(customer);
+    }
+
+    public List<Customer> getCustomers() {
+        return new ArrayList<>(customerList);
+    }
+
+    private String hash(String pin) {
+        // TODO: add actual hash function here
+        String salt = BCrypt.gensalt(12);
+        return BCrypt.hashpw(pin, salt);
+    }
+
+    public Customer findCustomer(int id) {
+        for (Customer customer : customerList) {
+            if (customer.getId() == id)
+                return customer;
+        }
+        return null;
+    }
+
+    public Customer checkPin(Customer inputCustomer) {
+        // 1. หา customer ที่มี id ตรงกับพารามิเตอร์
+        Customer storedCustomer = findCustomer(inputCustomer.getId());
+
+        // 2. ถ้ามี id ตรง ให้เช็ค pin ว่าตรงกันไหม โดยใช้ฟังก์ชันเกี่ยวกับ hash
+        if (storedCustomer != null) {
+            String hashPin = storedCustomer.getPin();
+
+            if (BCrypt.checkpw(inputCustomer.getPin(), hashPin))
+                return storedCustomer;
+        }
+        // 3. ถ้าไม่ตรง ต้องคืนค่า null
+        return null;
+    }
+
+}
